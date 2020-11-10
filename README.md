7de8eaeed1525ce1fd89a385bbbd50b68a9884a8
Kevin Gibbons <bakkot@gmail.com>
2020-11-09 21:10:59 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Transcribe to Google Docs
+
+This project lets you transcribe your audio input to Google Docs using Google's Cloud Speech API.
+
+The Cloud Speech API is not free, so you will need to provide your own credentials to use this project.
+
+
+## Setup
+
+### Local setup
+
+As with any node project, `npm install` to install dependencies.
+
+You will also need to install `sox` and have it available on your `$PATH`. On MacOS you can install it with Homebrew as `brew install sox`.
+
+
+### Project setup
+
+Create a Google Cloud project, enable the Speech-To-Text API for that project, create a service account, and aquire private key for that service account. You can do that all in one step by clicking the "Set up a project" button behind [this link](https://cloud.google.com/speech-to-text/docs/quickstart-client-libraries#before-you-begin). Then copy the JSON with the private key for the service account to a file named `speech-service-account-key.json` in this directory.
+
+Enable the Google Docs API for your project. You can do this from the [APIs & Services](https://console.developers.google.com/apis/dashboard) page on the Cloud console. Make sure you're in the right project at the top, then click "Enable APIs and Services", search for the Google Docs API, and click Enable.
+
+Set up the app to allow users to grant it permission to edit documents on their behalf. From the [APIs & Services](https://console.developers.google.com/apis/dashboard) page, click "OAuth Consent Screen" in the left menu, click "External", and press "Create". Provide a name and email to be displayed to users and a developer contact, then click Save and Continue. On the Scopes page, select the ".../auth/documents" scope ("View and manage your Google Docs documents"). Ignore the warning about verification for now. Click Save and Continue until you get to the Summary page. Don't worry about registration for now, just click Back to Dashboard.
+
+Finally, acquire the OAuth secret for your application. From the [APIs & Services](https://console.developers.google.com/apis/dashboard), select "Credentials", click Create Credentials at the top, select "OAuth Client ID", set the type to "Desktop app", give it a name, and click "create". It will tell you the ID and secret, but it's easier to click through and the click the download icon to the right of the newly created credential. Copy that JSON to a file named `gdocs-client-oauth-secret.json` in this directory.
+
+
+## Use
+
+`node run.js`
+
+This will ask for the ID of a Google Doc to transcribe to. This is the `1fOSJp9hZLoR2BhiE0da87bLtoBBgcvGdi063IU1wqW8` part of doc's URL, as in `https://docs.google.com/document/d/1fOSJp9hZLoR2BhiE0da87bLtoBBgcvGdi063IU1wqW8/edit`. Make sure it's a document you have permission to edit.
+
+If this is the first time running it, you'll then be asked to authorize an account. (The resulting credential is stored in `GENERATED_TOKEN.json`, so you should only need to do this once.) Go the URL it provides and grant permissions. You'll need to click through the big "This app isn't verified" warning (under "advanced"), unless you've verified your application. Ultimately you should get a string you will need to copy back to the terminal.
+
+That's it! It will then run forever, transcribing your microphone to the doc. Kill it with control-C when you're done.
+
+Keep in mind that this [costs money](https://cloud.google.com/speech-to-text/pricing). Specifically, since this is configured to use the fancy "video" model (which seemed to work best in my experience), it [will cost](https://cloud.google.com/speech-to-text/pricing#pricing_table) $0.009 every 15 seconds, or about $2.16 an hour.
+
+
+## Transcribing audio from your computer
+
+This records audio _in_, not audio _out_. So it won't directly help with, e.g., transcribing a stream you're viewing on your computer. You'll need to use a tool like [JACK](https://jackaudio.org/) or [BlackHole](https://github.com/ExistentialAudio/BlackHole) (MacOS only) to route your audio out to your audio in.
---
+++ run.js
@@ -0,0 +1,321 @@
+// This file is based in part on https://github.com/googleapis/nodejs-speech/blob/eafbadd3b22943dd1002c0f523f4d06d15bb9928/samples/infiniteStreaming.js
+
+'use strict';
+
+let fs = require('fs').promises;
+
+let { Writable } = require('stream');
+let recorder = require('node-record-lpcm16');
+let speech = require('@google-cloud/speech').v1p1beta1;
+let readline = require('readline');
+let { google } = require('googleapis');
+
+// TODO ask user to set this environment variable
+// this holds the speech API credentials (should be an object with `"type": "service_account"`)
+process.env.GOOGLE_APPLICATION_CREDENTIALS='./speech-service-account-key.json';
+
+// this holds the docs API OAuth secret
+const GDOCS_APPLICATION_SECRET_PATH = './gdocs-client-oauth-secret.json';
+
+// oauth token will be cached here
+const TOKEN_PATH = './GENERATED_TOKEN.json';
+
+
+
+let delay = ms => new Promise(res => setTimeout(res, ms));
+
+function ask(question) {
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise(res => {
+    rl.question(question, id => {
+      rl.close();
+      res(id);
+    });
+  });
+}
+
+
+async function infiniteStream(append, {
+  encoding = 'LINEAR16',
+  sampleRateHertz = 16000,
+  languageCode = 'en-US',
+  streamingLimit = 290000,
+} = {}) {
+
+  let client = new speech.SpeechClient();
+
+  let config = {
+    encoding,
+    sampleRateHertz,
+    languageCode,
+    enableAutomaticPunctuation: true,
+    model: 'video', // NB this is 50% more expensive
+  };
+
+  let request = {
+    config,
+    interimResults: true,
+  };
+
+  let recognizeStream = null;
+  let restartCounter = 0;
+  let audioInput = [];
+  let lastAudioInput = [];
+  let newStream = true;
+  let bridgingOffset = 0;
+
+  function startStream() {
+    audioInput = [];
+    recognizeStream = client
+      .streamingRecognize(request)
+      .on('error', err => {
+        if (err.code === 11) {
+          // restartStream();
+        } else {
+          console.error('API request error ' + err);
+        }
+      })
+      .on('data', speechCallback);
+
+    // Restart stream when streamingLimit expires
+    setTimeout(restartStream, streamingLimit);
+  }
+
+  // could we do this in a more clever way? yes. maybe we should eventually, but I don't care enough right now.
+  let queue = [];
+  let lock = false;
+  let prev = '';
+  setInterval(async () => {
+    if (queue.length === 0) {
+      return;
+    }
+    if (lock) {
+      return;
+    }
+    lock = true;
+
+    let next = queue.shift();
+
+    // Unfortunately the gdocs API's claimed `targetRevisionId` thing does not work at all, so we're stuck with just appending.
+    // The speech API doesn't finalize until a long time in, but early stuff tends not to change.
+    // So assume all but the last BUFFER characters can be committed. Sometimes this leads to weird typos, but it's worth it to have transcripts be more live.
+    let BUFFER = 20;
+    switch (next.type) {
+      case 'init': {
+        let text = next.text.substring(0, next.text.length - BUFFER);
+        console.log('init', JSON.stringify(text));
+        // await init(next.text);
+        await append(text);
+        prev = text;
+        break;
+      }
+      case 'update': {
+        let text = next.text.substring(0, next.text.length - BUFFER);
+        console.log('update', JSON.stringify(text));
+        // await update(next.text);
+        await append(text.substring(prev.length));
+        prev = text;
+
+        break;
+      }
+      case 'finish': {
+        console.log('finish', JSON.stringify(next.text));
+        // await finish(next.text);
+        await append(next.text.substring(prev.length));
+        prev = '';
+        break;
+      }
+    }
+
+    lock = false;
+  }, 1100);
+
+  let shouldInit = true;
+  let speechCallback = stream => {
+    let stdoutText = '';
+    if (stream.results[0].alternatives[0]) {
+      stdoutText = stream.results[0].alternatives[0].transcript;
+    }
+    if (stream.results[0].isFinal) {
+      queue.push({ type: 'finish', text: stdoutText });
+      shouldInit = true;
+    } else if (shouldInit) {
+      queue.push({ type: 'init', text: stdoutText });
+      shouldInit = false;
+    } else {
+      if (queue.length > 0 && queue[queue.length - 1].type === 'update') {
+        queue[queue.length - 1].text = stdoutText;
+      } else {
+        queue.push({ type: 'update', text: stdoutText });
+      }
+    }
+  };
+
+  let audioInputStreamTransform = new Writable({
+    write(chunk, encoding, next) {
+      if (newStream && lastAudioInput.length !== 0) {
+        // Approximate math to calculate time of chunks
+        const chunkTime = streamingLimit / lastAudioInput.length;
+        if (chunkTime !== 0) {
+          if (bridgingOffset < 0) {
+            bridgingOffset = 0;
+          }
+          if (bridgingOffset > finalRequestEndTime) {
+            bridgingOffset = finalRequestEndTime;
+          }
+          const chunksFromMS = Math.floor(
+            (finalRequestEndTime - bridgingOffset) / chunkTime
+          );
+          bridgingOffset = Math.floor(
+            (lastAudioInput.length - chunksFromMS) * chunkTime
+          );
+
+          for (let i = chunksFromMS; i < lastAudioInput.length; i++) {
+            recognizeStream.write(lastAudioInput[i]);
+          }
+        }
+        newStream = false;
+      }
+
+      audioInput.push(chunk);
+
+      if (recognizeStream) {
+        recognizeStream.write(chunk);
+      }
+
+      next();
+    },
+
+    final() {
+      if (recognizeStream) {
+        recognizeStream.end();
+      }
+    },
+  });
+
+  function restartStream() {
+    if (recognizeStream) {
+      recognizeStream.end();
+      recognizeStream.removeListener('data', speechCallback);
+      recognizeStream = null;
+    }
+
+    lastAudioInput = [];
+    lastAudioInput = audioInput;
+
+    restartCounter++;
+
+    process.stdout.write(`### ${streamingLimit * restartCounter}: RESTARTING REQUEST\n`);
+
+    newStream = true;
+
+    startStream();
+  }
+
+  // Start recording and send the microphone input to the Speech API
+  recorder
+    .record({
+      sampleRateHertz: sampleRateHertz,
+      threshold: 0, // Silence threshold
+      silence: 1000,
+      keepSilence: true,
+      recordProgram: 'rec', // Try also "arecord" or "sox"
+    })
+    .stream()
+    .on('error', err => {
+      console.error('Audio recording error ' + err);
+    })
+    .pipe(audioInputStreamTransform);
+
+  console.log('');
+  console.log('Listening, press Ctrl+C to stop.');
+  console.log('=========================================================');
+
+  startStream();
+}
+
+
+async function initGdocsClient(documentId) {
+  let secret = await fs.readFile(GDOCS_APPLICATION_SECRET_PATH, 'utf8');
+  let auth = await authorizeGdocsClient(JSON.parse(secret));
+
+  const docs = google.docs({ version: 'v1', auth });
+
+  try {
+    // The API doesn't seem to expose permissions queries
+    // So test for writing permissions by writing the empty string to the end of the document
+    await docs.documents.batchUpdate({
+      documentId,
+      requestBody: {
+        requests: [{
+          insertText: {
+            text: '',
+            endOfSegmentLocation: {
+              segmentId: '',
+            },
+          },
+        }],
+      },
+    });
+  } catch (e) {
+    if (!e.message.includes('Insert text requests must specify text to insert.')) {
+      console.error('Failed to write to document - do you have sufficient permissions?');
+      console.error('Message: ' + e.message);
+      process.exit(1);
+    }
+  }
+
+  return async text => {
+    if (text === '') {
+      return;
+    }
+    await docs.documents.batchUpdate({
+      documentId,
+      requestBody: {
+        requests: [{
+          insertText: {
+            text,
+            endOfSegmentLocation: {
+              segmentId: '',
+            },
+          },
+        }],
+      },
+    });
+  };
+};
+
+
+async function authorizeGdocsClient(credentials) {
+  let { client_secret, client_id, redirect_uris } = credentials.installed;
+  let oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  try {
+    let token = await fs.readFile(TOKEN_PATH, 'utf8');
+    oAuth2Client.setCredentials(JSON.parse(token));
+  } catch {
+    let authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: ['https://www.googleapis.com/auth/documents'],
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    let code = await ask('Enter the code from that page here: ');
+
+    let token = (await oAuth2Client.getToken(code)).tokens;
+    await fs.writeFile(TOKEN_PATH, JSON.stringify(token), 'utf8');
+    oAuth2Client.setCredentials(token);
+  }
+  return oAuth2Client;
+}
+
+(async () => {
+  let docId = await ask('Google Docs id: ');
+  let append = await initGdocsClient(docId.trim());
+  infiniteStream(append);  
+})().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
